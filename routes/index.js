955369a6933dd958c67477e8296000d35c95bf50
huanghaiyang <huanhai24@sina.com>
2015-04-20 12:34:59 +0800

    express与angularjs整合测试

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET home page. */
+router.get('/main', function(req, res, next) {
+  res.render('main', { title: 'Express' });
+});
+
+
 module.exports = router;
