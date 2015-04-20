b8afd64cfd28d5d725e9b1c766a481d2aca932d4
huanghaiyang <huanhai24@sina.com>
2015-04-20 16:34:20 +0800

    页面结构分解

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,24 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: '设计器首页' });
+});
+
+/* GET main page. */
+router.get('/main', function(req, res, next) {
+  res.render('main', { title: '主体操作区' });
 });
 
 /* GET home page. */
-router.get('/main', function(req, res, next) {
-  res.render('main', { title: 'Express' });
+router.get('/templates/:name', function(req, res, next) {
+	var name = req.params.name;
+	if (name === "generateTable") {
+		res.render('templates/generateTable', {
+			title: '生成表格模板',
+			rnumber: req.rnumber,
+			cnumber: req.cnumber
+		});
+	}
 });
 
 
